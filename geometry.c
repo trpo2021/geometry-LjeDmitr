3ca2b091455f866fb62b84f2b6db0bcb20563681
Dmitry Stacenko <dimastacenko104@gmail.com>
2021-03-01 21:59:25 +0700

    fix geometry.c

--- geometry.c
+++ geometry.c
@@ -3,209 +3,211 @@
 #include <stdlib.h>
 #include <string.h>
 
-int correct(char str[], int MAX)
+int correct(char input_str[])
 {
     char check_str[] = {'c', 'i', 'r', 'c', 'l', 'e'};
     int i = 0, j = 0, k = 0, g = 0;
-    while (str[i] == ' ') {
+    while (input_str[i] == ' ') {
         i++;
     }
-    for (; (str[i] != '(') && (isalpha(str[i]) > 0); i++, k++, g++) {
-        if (check_str[g] != (tolower(str[i]))) {
-            printf("Error at column %d '%c': expected 'circle'\n", i, str[i]);
+    for (; (input_str[i] != '(') && (isalpha(input_str[i]) > 0); i++, k++, g++) {
+        if (check_str[g] != (tolower(input_str[i]))) {
+            printf("Error at column %d '%c': expected 'circle'\n", i, input_str[i]);
             exit(0);
         }
     }
-    while (str[i] == ' ') {
+    while (input_str[i] == ' ') {
         i++;
     }
-    if (str[i] != '(') {
-        printf("Error at column %d '%c': expected '('\n", i, str[i]);
+    if (input_str[i] != '(') {
+        printf("Error at column %d '%c': expected '('\n", i, input_str[i]);
         exit(0);
     } else {
         i++;
         k++;
     }
-    while ((isdigit(str[i]) == 0) && (str[i] != '-')) {
-        if (str[i] == ' ') {
+    while ((isdigit(input_str[i]) == 0) && (input_str[i] != '-')) {
+        if (input_str[i] == ' ') {
             i++;
         } else {
-            printf("Error at column %d '%c': expected '<double>'\n", i, str[i]);
+            printf("Error at column %d '%c': expected '<double>'\n", i, input_str[i]);
             exit(0);
         }
     }
-    if (str[i] == '-') {
+    if (input_str[i] == '-') {
         i++;
         k++;
     }
-    if (isdigit(str[i]) == 0) {
-        printf("Error at column %d '%c': expected '<double>'\n", i, str[i]);
+    if (isdigit(input_str[i]) == 0) {
+        printf("Error at column %d '%c': expected '<double>'\n", i, input_str[i]);
         exit(0);
     }
-    while ((isdigit(str[i]) > 0) || (str[i] == '.')) {
-        if ((str[i] == '.') && (j == 0)) {
+    while ((isdigit(input_str[i]) > 0) || (input_str[i] == '.')) {
+        if ((input_str[i] == '.') && (j == 0)) {
             j++;
         }
         i++;
         k++;
-        if ((str[i] == '.') && (j > 0)) {
-            printf("Error at column %d '%c': expected '<double>'\n", i, str[i]);
+        if ((input_str[i] == '.') && (j > 0)) {
+            printf("Error at column %d '%c': expected '<double>'\n", i, input_str[i]);
             exit(0);
         }
     }
     k++;
-    while ((isdigit(str[i]) == 0) && (str[i] != '-')) {
-        if (str[i] == ' ') {
+    while ((isdigit(input_str[i]) == 0) && (input_str[i] != '-')) {
+        if (input_str[i] == ' ') {
             i++;
         } else {
-            printf("Error at column %d '%c'", i, str[i]);
+            printf("Error at column %d '%c'", i, input_str[i]);
             printf(": expected '<double>' or 'space'\n");
             exit(0);
         }
     }
-    if (str[i] == '-') {
+    if (input_str[i] == '-') {
         i++;
         k++;
     }
 
-    if (isdigit(str[i]) == 0) {
-        printf("Error at column %d '%c': expected '<double>'\n", i, str[i]);
+    if (isdigit(input_str[i]) == 0) {
+        printf("Error at column %d '%c': expected '<double>'\n", i, input_str[i]);
         exit(0);
     }
     j = 0;
-    while ((isdigit(str[i]) > 0) || (str[i] == '.')) {
-        if ((str[i] == '.') && (j == 0)) {
+    while ((isdigit(input_str[i]) > 0) || (input_str[i] == '.')) {
+        if ((input_str[i] == '.') && (j == 0)) {
             j = i;
         }
         i++;
         k++;
-        if ((str[i] == '.') && (j > 0)) {
-            printf("Error at column %d '%c': expected '<double>'\n", i, str[i]);
+        if ((input_str[i] == '.') && (j > 0)) {
+            printf("Error at column %d '%c': expected '<double>'\n", i, input_str[i]);
             exit(0);
         }
     }
-    if ((str[j] == '.') && (isdigit(str[j + 1]) == 0)) {
-        printf("Error at column %d '%c': expected '<double>'\n", i, str[i]);
+    if ((input_str[j] == '.') && (isdigit(input_str[j + 1]) == 0)) {
+        printf("Error at column %d '%c': expected '<double>'\n", i, input_str[i]);
         exit(0);
     }
-    while (str[i] == ' ') {
+    while (input_str[i] == ' ') {
         i++;
     }
-    if (str[i] != ',') {
-        printf("Error at column %d '%c': expected ','\n", i, str[i]);
+    if (input_str[i] != ',') {
+        printf("Error at column %d '%c': expected ','\n", i, input_str[i]);
         exit(0);
     } else {
         i++;
         k++;
     }
     k++;
-    while (isdigit(str[i]) == 0) {
-        if (str[i] == ' ') {
+    while (isdigit(input_str[i]) == 0) {
+        if (input_str[i] == ' ') {
             i++;
         } else {
-            printf("Error at column %d '%c'", i, str[i]);
+            printf("Error at column %d '%c'", i, input_str[i]);
             printf(": expected '<double>' or 'space'\n");
             exit(0);
         }
     }
     j = 0;
-    while ((isdigit(str[i]) > 0) || (str[i] == '.')) {
-        if ((str[i] == '.') && (j == 0)) {
+    while ((isdigit(input_str[i]) > 0) || (input_str[i] == '.')) {
+        if ((input_str[i] == '.') && (j == 0)) {
             j = i;
         }
         i++;
         k++;
-        if ((str[i] == '.') && (j > 0)) {
-            printf("Error at column %d '%c': expected '<double>'\n", i, str[i]);
+        if ((input_str[i] == '.') && (j > 0)) {
+            printf("Error at column %d '%c': expected '<double>'\n", i, input_str[i]);
             exit(0);
         }
     }
-    if ((str[j] == '.') && (isdigit(str[j + 1]) == 0)) {
-        printf("Error at column %d '%c': expected '<double>'\n", i, str[i]);
+    if ((input_str[j] == '.') && (isdigit(input_str[j + 1]) == 0)) {
+        printf("Error at column %d '%c': expected '<double>'\n", i, input_str[i]);
         exit(0);
     }
-    while (str[i] == ' ') {
+    while (input_str[i] == ' ') {
         i++;
     }
-    if (str[i] != ')') {
-        printf("Error at column %d '%c': expected ')'\n", i, str[i]);
+    if (input_str[i] != ')') {
+        printf("Error at column %d '%c': expected ')'\n", i, input_str[i]);
         exit(0);
     } else {
         i++;
         k++;
     }
-    while (str[i] == ' ') {
+    while (input_str[i] == ' ') {
         i++;
     }
-    if (str[i] != '\n') {
-        printf("Error at column %d '%c': unexpected token\n", i, str[i]);
+    if (input_str[i] != '\n') {
+        printf("Error at column %d '%c': unexpected token\n", i, input_str[i]);
         exit(0);
     }
     printf("Input is correct\n(case insensitive and ignoring whitespace)\n");
     return k;
 }
 
-void str_copy(char str[], char* str_1, int MAX)
+void str_copy(char input_str[], char* output_str)
 {
     int i = 0, j;
-    while (str[i] == ' ') {
+    while (input_str[i] == ' ') {
         i++;
     }
-    for (j = 0; (str[i] != '(') && (str[i] != ' '); i++, j++) {
-        str_1[j] = str[i];
+    for (j = 0; (input_str[i] != '(') && (input_str[i] != ' '); i++, j++) {
+        output_str[j] = input_str[i];
     }
-    str_1[j] = '(';
+    output_str[j] = '(';
     j++;
-    while ((str[i] != '-') && (isdigit(str[i]) == 0)) {
+    while ((input_str[i] != '-') && (isdigit(input_str[i]) == 0)) {
         i++;
     }
-    for (; str[i] != ' '; i++, j++) {
-        str_1[j] = str[i];
+    for (; input_str[i] != ' '; i++, j++) {
+        output_str[j] = input_str[i];
     }
-    str_1[j] = ' ';
+    output_str[j] = ' ';
     j++;
-    while (str[i] == ' ') {
+    while (input_str[i] == ' ') {
         i++;
     }
-    for (; (str[i] != ',') && (str[i] != ' '); i++, j++) {
-        str_1[j] = str[i];
+    for (; (input_str[i] != ',') && (input_str[i] != ' '); i++, j++) {
+        output_str[j] = input_str[i];
     }
-    str_1[j] = ',';
+    output_str[j] = ',';
     j++;
-    str_1[j] = ' ';
+    output_str[j] = ' ';
     j++;
-    while (str[i] == ' ') {
+    while (input_str[i] == ' ') {
         i++;
     }
-    if (str[i] == ',') {
+    if (input_str[i] == ',') {
         i++;
     }
-    while (str[i] == ' ') {
+    while (input_str[i] == ' ') {
         i++;
     }
-    for (; (str[i] != ')') && (str[i] != ' '); i++, j++) {
-        str_1[j] = str[i];
+    for (; (input_str[i] != ')') && (input_str[i] != ' '); i++, j++) {
+        output_str[j] = input_str[i];
     }
-    str_1[j] = ')';
+    output_str[j] = ')';
+    j++;
+    output_str[j] = '\0';
 }
 
-void output(char* str)
+void output(char* output_str)
 {
-    fputs(str, stdout);
+    fputs(output_str, stdout);
 }
 
 int main()
 {
-    int MAX = 1000, size;
-    char* str = NULL;
-    str = calloc(MAX, sizeof(char));
-    char* str_1 = NULL;
-    fgets(str, MAX, stdin);
-    size = correct(str, MAX);
-    str_1 = calloc(size, sizeof(char));
-    str_copy(str, str_1, MAX);
-    output(str_1);
-    free(str);
+    int max_str = 1000, size_output_str;
+    char* input_str = NULL;
+    input_str = calloc(max_str, sizeof(char));
+    char* output_str = NULL;
+    fgets(input_str, max_str, stdin);
+    size_output_str = correct(input_str);
+    output_str = calloc(size_output_str, sizeof(char));
+    str_copy(input_str, output_str);
+    output(output_str);
+    free(input_str);
     printf("\n");
     return 0;
 }
